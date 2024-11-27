2b6268c689a82f410e7fc2e5084a0aef970a00e6
malouVich <malouvichlavrentiew@gmail.com>
2024-11-27 13:41:31 +0100

    tilføjet metoden showOrders i OrderController

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,18 +1,33 @@
 package app.controllers;
 
+import app.entities.Order;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.OrderMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+
 public class OrderController
 {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
-    { app.get("bestil", ctx -> showUnassignedOrders(ctx, connectionPool));
+    { app.get("bestil", ctx -> showOrders(ctx, connectionPool));
 
     }
 
-    private static void showUnassignedOrders(Context ctx, ConnectionPool connectionPool)
+    private static void showOrders(Context ctx, ConnectionPool connectionPool)
     {
+        try {
+            ArrayList<Order> orders = OrderMapper.getOrders(connectionPool);
+
+            ctx.attribute("bottoms", orders);
+            ctx.render("bestil.html");
+
+        } catch (DatabaseException e) {
+            ctx.attribute("errorMessage", "Der var et problem ved at hente siden pga. fejl ved at hente data.");
+            ctx.render("errorAlreadyLogin.html");
+        }
     }
 }
