ab3f1c8888cf130a938a36e6f9447438005c799a
malouVich <malouvichlavrentiew@gmail.com>
2024-11-27 14:09:50 +0100

    tilføjet atributter til order

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -1,5 +1,33 @@
 package app.entities;
 
 public class Order
-{
+{   private int id;
+    private String status;
+    private String customer;
+    private double total;
+
+
+    public Order(int id, String status, String customer, double total)
+    {
+        this.id = id;
+        this.status = status;
+        this.customer = customer;
+        this.total = total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public double getTotal() {
+        return total;
+    }
 }
