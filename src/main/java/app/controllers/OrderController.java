c63d809300619132f9247f885ad1da99923836af
malouVich <malouvichlavrentiew@gmail.com>
2024-11-27 13:16:03 +0100

    tilføjet addroutes til OrderController

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,5 +1,18 @@
 package app.controllers;
 
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+
 public class OrderController
 {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
+    { app.get("bestil", ctx -> showUnassignedOrders(ctx, connectionPool));
+
+    }
+
+    private static void showUnassignedOrders(Context ctx, ConnectionPool connectionPool)
+    {
+    }
 }
