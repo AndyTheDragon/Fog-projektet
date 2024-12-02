ed542ac35cf70dc483922e29cf168df406cf128c
AndyTheDragon <andre.jansam@gmail.com>
2024-12-02 12:06:47 +0100

    test af ny WorkDrawing constructor

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -41,10 +41,10 @@ public class OrderController
     private static void showDrawing(Context ctx, ConnectionPool dbConnection)
     {
         Carport carport = new Carport(780,600,210,530, RoofType.FLAT);
-        WorkDrawing drawing = new WorkDrawing(carport.getLength(), carport.getWidth(), 230, carport.getShedLength(), carport.getShedWidth(), carport.getNumberOfJoists());
+        WorkDrawing drawing = new WorkDrawing(carport.getLength(), carport.getWidth(), 230, carport.getShedLength(), carport.getShedWidth(), carport.getNumberOfJoists(), carport.extraPostsForLongCarport());
         ctx.attribute("drawing", drawing.toString());
         Carport carport2 = new Carport(480,300,0,0, RoofType.FLAT);
-        WorkDrawing noShed = new WorkDrawing(carport2.getLength(), carport2.getWidth(), 230, carport2.getNumberOfJoists());
+        WorkDrawing noShed = new WorkDrawing(carport2, 480);
         ctx.attribute("noshed", noShed.toString());
 
         ctx.render("drawing.html");
