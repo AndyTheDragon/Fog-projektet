0e1d114c23c86b378df1f9abd5219b7eb224e1b8
Falcho <m.k.falcham@gmail.com>
2024-12-03 11:21:56 +0100

    Gave life to BSB class by addin attributes, constructor and toString

--- src/main/java/app/entities/BoltsScrewsBrackets.java
+++ src/main/java/app/entities/BoltsScrewsBrackets.java
@@ -2,4 +2,34 @@ package app.entities;
 
 public class BoltsScrewsBrackets
 {
+    private int length;
+    private int width;
+    private int amount;
+    private String type;
+    private String unit;
+    private String description;
+    private int fogProductID;
+
+    public BoltsScrewsBrackets(int amount, String description, int length, String type, String unit, int width)
+    {
+        this.amount = amount;
+        this.description = description;
+        this.length = length;
+        this.type = type;
+        this.unit = unit;
+        this.width = width;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "BoltsScrewsBrackets{" +
+                "amount=" + amount +
+                ", description='" + description + '\'' +
+                ", length=" + length +
+                ", type='" + type + '\'' +
+                ", width=" + width +
+                ", unit='" + unit + '\'' +
+                '}';
+    }
 }
