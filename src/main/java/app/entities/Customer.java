7e6656052be8e2bc9bd04f5726f0f548b706bc7a
MojoOno <Ffranck0406@gmail.com>
2024-12-04 11:32:46 +0100

    Created Customer class with constructor and getters according to classDiagram and database

--- src/main/java/app/entities/Customer.java
+++ src/main/java/app/entities/Customer.java
@@ -2,4 +2,59 @@ package app.entities;
 
 public class Customer
 {
+    int customerID;
+    String name;
+    String address;
+    String zipcode;
+    String city;
+    String phoneNumber;
+    String email;
+
+    public Customer(String name, String address, String zipcode, String city, String phoneNumber, String email)
+    {
+        this.name = name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getCustomerID()
+    {
+        return customerID;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public String getZipcode()
+    {
+        return zipcode;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
 }
+
+
+
