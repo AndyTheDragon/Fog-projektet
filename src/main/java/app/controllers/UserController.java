1ec744ca810a54f2d619fe463335b10d8e26ee9e
malouVich <malouvichlavrentiew@gmail.com>
2024-12-05 14:29:04 +0100

    tilføjet addRoutes i usercontroller og ændret til orders i redirect

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -4,25 +4,29 @@ import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.UserMapper;
+import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 public class UserController
 {
+    public static void addRoutes(Javalin app, ConnectionPool dbconnection) {
+        app.get("/", ctx -> ctx.render("login.html"));
+        app.post("/login", ctx -> login(ctx, dbconnection));
 
-
-    public static void login(Context ctx, ConnectionPool connectionPool) {
+    }
+    public static void login(Context ctx, ConnectionPool dbconnection) {
         String email = ctx.formParam("email");
         String password = ctx.formParam("password");
 
         try {
-            User user = UserMapper.login(email, password, connectionPool);
+            User user = UserMapper.login(email, password, dbconnection);
             ctx.sessionAttribute("currentUser", user);
 
-            ctx.redirect("ordreoversigt");
+            ctx.redirect("/orders");
 
         } catch (DatabaseException e) {
             ctx.attribute("message", e.getMessage());
-            ctx.render("login.html");
+            ctx.render("index.html");
         }
     }
 }
