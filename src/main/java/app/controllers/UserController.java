3429a343eeb42fe31c282c0568519b6fc8a0f386
malouVich <malouvichlavrentiew@gmail.com>
2024-12-05 13:09:07 +0100

    tilføjet login i UserController

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,5 +1,28 @@
 package app.controllers;
 
+import app.entities.User;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.UserMapper;
+import io.javalin.http.Context;
+
 public class UserController
 {
+
+
+    public static void login(Context ctx, ConnectionPool connectionPool) {
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");
+
+        try {
+            User user = UserMapper.login(email, password, connectionPool);
+            ctx.sessionAttribute("currentUser", user);
+
+            ctx.redirect("ordreoversigt");
+
+        } catch (DatabaseException e) {
+            ctx.attribute("message", e.getMessage());
+            ctx.render("login.html");
+        }
+    }
 }
