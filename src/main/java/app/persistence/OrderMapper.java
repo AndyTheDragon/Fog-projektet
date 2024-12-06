ec18322797009a4ab92d7c5e813d5b2c6c692a63
Ingridksv <ingrid.s.96@hotmail.com>
2024-12-06 13:24:27 +0100

    createOrder method

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -6,10 +6,7 @@ import app.entities.RoofType;
 import app.entities.User;
 import app.exceptions.DatabaseException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
