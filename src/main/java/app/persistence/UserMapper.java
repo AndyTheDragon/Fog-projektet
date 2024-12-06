879dacf952effffed8d2df9bf4de9a970979d413
AndyTheDragon <andre.jansam@gmail.com>
2024-12-06 11:46:42 +0100

    Code formatting

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -11,25 +11,32 @@ import java.sql.SQLException;
 public class UserMapper
 {
 
-    public static User login(String email, String password, ConnectionPool dbconnection) throws DatabaseException {
+    public static User login(String email, String password, ConnectionPool dbconnection) throws DatabaseException
+    {
         String sql = "SELECT * FROM account WHERE email=? AND user_password=?";
 
         try (Connection connection = dbconnection.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql)) {
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
 
             ps.setString(1, email);
             ps.setString(2, password);
 
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
+            if (rs.next())
+            {
                 int userId = rs.getInt("user_id");
                 String userName = rs.getString("user_name");
                 boolean isAdmin = rs.getBoolean("is_admin");
                 return new User(userId, userName, email, password, isAdmin);
-            } else {
+            }
+            else
+            {
                 throw new DatabaseException("Forkert email eller adgangskode");
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new DatabaseException("Message "+ e.getMessage());
         }
     }
