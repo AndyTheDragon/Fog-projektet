ceb7aa2069d96935d584b7da5072f3df831cd7cf
AndyTheDragon <andre.jansam@gmail.com>
2024-12-07 18:37:39 +0100

    Tilføjet metoder til IMaterials.

--- src/main/java/app/entities/IMaterials.java
+++ src/main/java/app/entities/IMaterials.java
@@ -2,4 +2,9 @@ package app.entities;
 
 public interface IMaterials
 {
+    public int getAmount();
+    public String getDescription();
+    public int getLength();
+    public String getType();
+    public String getUnit();
 }
