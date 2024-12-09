90f820c661b0ea69de4032388caecef0973f7845
Falcho <m.k.falcham@gmail.com>
2024-12-09 10:51:48 +0100

    Made second materialMapper method that gets all types of a type of wood, with a minimum length of input

--- src/main/java/app/persistence/MaterialMapper.java
+++ src/main/java/app/persistence/MaterialMapper.java
@@ -47,4 +47,34 @@ public class MaterialMapper
         return null;
     }
 
+    public List<IMaterials> getMaterialOfTypeAndLength(ConnectionPool connectionPool, String type, int minLength)
+    {
+        List<IMaterials> materialsList = new ArrayList<>();
+        String sql = "SELECT m.material_id, m.material_name, m.width, m.height, m.length, f.description FROM carport_material AS m" +
+                " INNER JOIN carport_material_function ON m.material_id = carport_material_function.material_id" +
+                " INNER JOIN material_function AS f ON carport_material_function.function_id=f.function_id" +
+                " WHERE f.description = ? AND m.length >= ?";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, type);
+            ps.setInt(2, minLength);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int height = rs.getInt("height");
+                int width = rs.getInt("width");
+                int length = rs.getInt("length");
+                int materialId = rs.getInt("material_id");
+                String materialName = rs.getString("material_name");
+                String description = rs.getString("description");
+                materialsList.add(new ConstructionWood(height, width, length, "stk", materialName, description, 0, materialId));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return materialsList;
+    }
+
 }
