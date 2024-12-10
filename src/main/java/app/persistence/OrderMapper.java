4d835ec36f3c77044700b14df596a27b21cd0b20
Falcho <m.k.falcham@gmail.com>
2024-12-10 10:05:03 +0100

    Refactored OptimalWood to Services package

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -2,6 +2,7 @@ package app.persistence;
 
 import app.entities.*;
 import app.exceptions.DatabaseException;
+import app.services.OptimalWoodCalculator;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
