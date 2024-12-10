7ab84cbd9915629fdfde516fa2063d5f38685a88
AndyTheDragon <andre.jansam@gmail.com>
2024-12-10 23:58:00 +0100

    ryddet op

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -21,28 +21,22 @@ public class OrderController
 {
     public static void addRoutes(Javalin app, ConnectionPool dbConnection)
     {
+        app.get("/bestilling", ctx -> showRequestPage(ctx, dbConnection));
+        app.post("/bestilling", ctx -> createOrder(ctx, dbConnection));
         app.get("/draw", ctx -> showDrawing(ctx, dbConnection));
-        app.get("/orders", ctx -> showOrders(ctx, dbConnection));
+        app.get("/orders", ctx -> showOrders(0, ctx, dbConnection));
+        app.get("/orders/{salesId}", ctx -> showMyOrders(ctx, dbConnection));
         app.get("/order/{orderId}", ctx -> showOrderDetails(ctx,dbConnection));
         app.post("/order/accept", ctx -> acceptOrder(ctx,dbConnection));
         app.post("/order/requestchange", ctx -> requestChange(ctx,dbConnection));
         app.post("/order/assign",ctx -> assignOrder(ctx,dbConnection));
         app.get("/order/{orderId}/edit", ctx -> editOrder(ctx, dbConnection));
-        //app.get("/login", ctx -> showLogin(ctx));
-        //app.post("/login", ctx -> doLogin(ctx,dbConnection));
-        app.get("/bestilling", ctx -> showOrderPage(ctx, dbConnection));
-        app.post("/bestilling", ctx -> createOrder(ctx, dbConnection));
+
 
     }
 
-    public static void showOrderPage(Context ctx, ConnectionPool dbConnection)
+    public static void showRequestPage(Context ctx, ConnectionPool dbConnection)
     {
-        Order currentOrder = ctx.sessionAttribute("currentOrder");
-        if (currentOrder == null) {
-            ctx.attribute("message", "Du har ikke nogen ordre i gang");
-        } else {
-            ctx.attribute("order", currentOrder);
-        }
         ctx.render("bestilling.html");
     }
 
@@ -54,7 +48,7 @@ public class OrderController
             int shedWidth = Integer.parseInt(Objects.requireNonNull(ctx.formParam("shedWidth")));
             int shedLength = Integer.parseInt(Objects.requireNonNull(ctx.formParam("shedLength")));
 
-            RoofType carportRoof = ctx.formParam("carportRoof").equals("flat") ? RoofType.FLAT : RoofType.FLAT;
+            RoofType carportRoof = "flat".equals(ctx.formParam("carportRoof")) ? RoofType.FLAT : RoofType.FLAT;
             boolean isPaid = false;
 
             Customer customer = new Customer(ctx.formParam("name"), ctx.formParam("address"), ctx.formParam("zipcode"), ctx.formParam("city"), ctx.formParam("phoneNumber"), ctx.formParam("email"));
@@ -62,9 +56,7 @@ public class OrderController
             customer.setCustomerID(customerID);
 
             Order order = new Order(0, customer, new User(), carportWidth, carportLength,
-                    shedWidth, shedLength, carportRoof, isPaid, LocalDateTime.now(), LocalDateTime.now(), new OptimalWoodCalculator(carportLength, carportWidth, shedLength, shedWidth, dbConnection));
-            //OrderMapper.createOrder(order, dbConnection);
-
+                    shedWidth, shedLength, carportRoof, isPaid, LocalDateTime.now(), LocalDateTime.now(), OrderStatus.UNASSIGNED, new OptimalWoodCalculator(carportLength, carportWidth, shedLength, shedWidth, dbConnection));
             OrderMapper.saveOrderToDatabase(order, dbConnection);
             ctx.attribute("message", "Ordren blev oprettet med succes.");
             ctx.render("kvittering.html");
@@ -77,82 +69,54 @@ public class OrderController
         }
     }
 
-    private static void doLogin(@NotNull Context ctx, ConnectionPool dbConnection)
+    private static void showDrawing(Context ctx, ConnectionPool dbConnection)
     {
-        //US-3
+        Carport carport = new Carport(780,600,210,530, RoofType.FLAT, new OptimalWoodCalculator(780, 600, 210, 530, dbConnection));
+        WorkDrawing drawing = new WorkDrawing(carport,780);
+        ctx.attribute("drawing", drawing.toString());
+        Carport carport2 = new Carport(480,300,0,0, RoofType.FLAT, new OptimalWoodCalculator(480, 300, 0, 0, dbConnection));
+        WorkDrawing noShed = new WorkDrawing(carport2, 480);
+        ctx.attribute("noshed", noShed.toString());
+
+        ctx.render("drawing.html");
+
     }
 
-    private static void showLogin(@NotNull Context ctx)
+    private static void showMyOrders(Context ctx, ConnectionPool dbConnection)
     {
-        //US-3
+        int salesId = Integer.parseInt(ctx.pathParam("salesId"));
+        showOrders(salesId, ctx, dbConnection);
     }
 
-    private static void assignOrder(@NotNull Context ctx, ConnectionPool dbConnection)
+    private static void showOrders(int salesId, Context ctx, ConnectionPool connectionPool)
     {
+        ctx.sessionAttribute("currentUser", new User());
         User currentUser = ctx.sessionAttribute("currentUser");
-
-        try
-        {
-            int orderId = Integer.parseInt(ctx.formParam("orderId"));
-            Order order = OrderMapper.getOrder(orderId,dbConnection);
-            if (order.getSalesPerson() != null)
-            {
-                ctx.attribute("message", "Ordren er allerede tildelt");
-            }
-            else
-            {
-                int salesId = (currentUser == null ? 0 : currentUser.getUserID());
-                if (salesId > 0)
-                {
-                    OrderMapper.asssignOrder(orderId, salesId, dbConnection);
-                    ctx.attribute("message", "Ordren blev tildelt");
-                    ctx.redirect("/order/"+orderId);
-                }
-                else
-                {
-                    ctx.attribute("message", "Du skal være logget ind for at tildel ordre");
-                }
-            }
-        }
-        catch (NumberFormatException e)
-        {
-            ctx.attribute("message", "Ugyldigt ordre id");
-        }
-        catch (DatabaseException e)
-        {
-            ctx.attribute("message", "Databasefejl: " + e.getMessage());
-        }
-        showOrders(ctx, dbConnection);
-
-    }
-
-    private static void editOrder(@NotNull Context ctx, ConnectionPool dbConnection)
-    {
-        int orderId = 0;
-        Order order = null;
-        if (ctx.sessionAttribute("currentUser") != null)
+        if (currentUser != null)
         {
             try
             {
-                orderId = Integer.parseInt(ctx.pathParam("orderId"));
-                order = OrderMapper.getOrder(orderId,dbConnection);
-            }
-            catch (NumberFormatException e)
-            {
-                ctx.attribute("message", "Invalid order id");
+
+                Map<String, ArrayList<Order>> orders = OrderMapper.getOrders(salesId, connectionPool);
+
+                ArrayList<Order> assigned = orders.get("assigned");
+                ArrayList<Order> unassigned = orders.get("unassigned");
+
+                ctx.attribute("assigned", assigned);
+                ctx.attribute("unassigned", unassigned);
+
+
             }
             catch (DatabaseException e)
             {
-                ctx.attribute("message", "Database error. " + e.getMessage());
+                ctx.attribute("message", "Fejl ved hentning af ordrer: " + e.getMessage());
+            }
+            catch (NumberFormatException e)
+            {
+                ctx.attribute("message", "Ugyldigt sælger id");
             }
         }
-        else
-        {
-            ctx.attribute("message", "Du har ikke adgang til denne side");
-            ctx.render("kvittering.html");
-        }
-        ctx.attribute("order", order);
-        ctx.render("ordreredigering.html");
+        ctx.render("ordreoversigt.html");
     }
 
     private static void showOrderDetails(@NotNull Context ctx, ConnectionPool dbConnection)
@@ -192,7 +156,6 @@ public class OrderController
         ctx.attribute("order", order);
         ctx.render("ordredetaljer.html");
 
-
     }
 
     private static void acceptOrder(@NotNull Context ctx, ConnectionPool dbConnection)
@@ -203,7 +166,7 @@ public class OrderController
             ctx.attribute("message", "Du har ikke adgang til denne side");
             ctx.render("kvittering.html");
 
-            //Der skal lige kigges på det her
+            //TODO: Der skal lige kigges på det her
         }
         try
         {
@@ -250,43 +213,72 @@ public class OrderController
         ctx.render("afvisttilbud.html");
     }
 
-    private static void showDrawing(Context ctx, ConnectionPool dbConnection)
-    {
-        Carport carport = new Carport(780,600,210,530, RoofType.FLAT, new OptimalWoodCalculator(780, 600, 210, 530, dbConnection));
-        WorkDrawing drawing = new WorkDrawing(carport,780);
-        ctx.attribute("drawing", drawing.toString());
-        Carport carport2 = new Carport(480,300,0,0, RoofType.FLAT, new OptimalWoodCalculator(480, 300, 0, 0, dbConnection));
-        WorkDrawing noShed = new WorkDrawing(carport2, 480);
-        ctx.attribute("noshed", noShed.toString());
 
-        ctx.render("drawing.html");
+    private static void assignOrder(@NotNull Context ctx, ConnectionPool dbConnection)
+    {
+        User currentUser = ctx.sessionAttribute("currentUser");
+
+        try
+        {
+            int orderId = Integer.parseInt(ctx.formParam("orderId"));
+            Order order = OrderMapper.getOrder(orderId,dbConnection);
+            if (order.getSalesPerson() != null)
+            {
+                ctx.attribute("message", "Ordren er allerede tildelt");
+            }
+            else
+            {
+                int salesId = (currentUser == null ? 0 : currentUser.getUserID());
+                if (salesId > 0)
+                {
+                    OrderMapper.asssignOrder(orderId, salesId, dbConnection);
+                    ctx.attribute("message", "Ordren blev tildelt");
+                    ctx.redirect("/order/"+orderId);
+                }
+                else
+                {
+                    ctx.attribute("message", "Du skal være logget ind for at tildel ordre");
+                }
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            ctx.attribute("message", "Ugyldigt ordre id");
+        }
+        catch (DatabaseException e)
+        {
+            ctx.attribute("message", "Databasefejl: " + e.getMessage());
+        }
+        showOrders(0, ctx, dbConnection);
 
     }
 
-    private static void showOrders(Context ctx, ConnectionPool connectionPool)
+    private static void editOrder(@NotNull Context ctx, ConnectionPool dbConnection)
     {
-        User currentUser = ctx.sessionAttribute("currentUser");
-        currentUser = new User();
-        if (currentUser != null)
+        int orderId = 0;
+        Order order = null;
+        if (ctx.sessionAttribute("currentUser") != null)
         {
             try
             {
-                Map<String, ArrayList<Order>> orders = OrderMapper.getOrders(connectionPool);
-
-                ArrayList<Order> assigned = orders.get("assigned");
-                ArrayList<Order> unassigned = orders.get("unassigned");
-
-                ctx.attribute("assigned", assigned);
-                ctx.attribute("unassigned", unassigned);
-
-
+                orderId = Integer.parseInt(ctx.pathParam("orderId"));
+                order = OrderMapper.getOrder(orderId,dbConnection);
+            }
+            catch (NumberFormatException e)
+            {
+                ctx.attribute("message", "Invalid order id");
             }
             catch (DatabaseException e)
             {
-                ctx.attribute("message", "Fejl ved hentning af ordrer: " + e.getMessage());
+                ctx.attribute("message", "Database error. " + e.getMessage());
             }
         }
-        ctx.render("ordreoversigt.html");
+        else
+        {
+            ctx.attribute("message", "Du har ikke adgang til denne side");
+            ctx.render("kvittering.html");
+        }
+        ctx.attribute("order", order);
+        ctx.render("ordreredigering.html");
     }
-
 }
