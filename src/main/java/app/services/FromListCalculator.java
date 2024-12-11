09077bbcdf6591076b1b5555259a61ada01fa820
malouVich <malouvichlavrentiew@gmail.com>
2024-12-11 19:22:20 +0100

    opdateret calcNumberOfCladdingBoards metoden

--- src/main/java/app/services/FromListCalculator.java
+++ src/main/java/app/services/FromListCalculator.java
@@ -118,7 +118,14 @@ public class FromListCalculator implements CarportCalculator
     @Override
     public int calcNumberOfCladdingBoards()
     {
-        return 0;
+        if (materialsList == null || materialsList.isEmpty())
+        {
+            return 0;
+        }
+
+        return (int) materialsList.stream()
+                .filter(material -> "beklædning".equals(material.getDescription()))
+                .count();
     }
 
     @Override
