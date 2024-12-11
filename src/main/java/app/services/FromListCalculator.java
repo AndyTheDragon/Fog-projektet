6e4c75e966ede88c29ea78f8bcb5ff2f753c00d0
malouVich <malouvichlavrentiew@gmail.com>
2024-12-11 17:53:58 +0100

    opdateret calcJoistBracketScrews metoden

--- src/main/java/app/services/FromListCalculator.java
+++ src/main/java/app/services/FromListCalculator.java
@@ -170,7 +170,10 @@ public class FromListCalculator implements CarportCalculator
     @Override
     public List<IMaterials> calcJoistBracketScrews() throws CalculatorException
     {
-        return List.of();
+        if(materialsList == null || materialsList.isEmpty()) throw new CalculatorException("materialList is null or empty");
+        return materialsList.stream()
+                .filter(material -> "Til montering af universalbeslag + hulbånd".equalsIgnoreCase(material.getDescription()))
+                .collect(Collectors.toList());
     }
 
     @Override
