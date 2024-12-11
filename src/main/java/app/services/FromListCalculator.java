64335beefa2a87933ac425d2f6ba5cd5896b3be3
malouVich <malouvichlavrentiew@gmail.com>
2024-12-11 17:09:37 +0100

    opdateret calcCladding metoden

--- src/main/java/app/services/FromListCalculator.java
+++ src/main/java/app/services/FromListCalculator.java
@@ -102,7 +102,10 @@ public class FromListCalculator implements CarportCalculator
     @Override
     public List<IMaterials> calcCladding() throws CalculatorException
     {
-        return List.of();
+        if(materialsList == null || materialsList.isEmpty()) throw new CalculatorException("materialList is null or empty");
+        return materialsList.stream()
+                .filter(material -> "beklædning".equalsIgnoreCase(material.getDescription()))
+                .collect(Collectors.toList());
     }
 
     @Override
