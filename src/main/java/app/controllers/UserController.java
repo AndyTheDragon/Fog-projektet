16088348f5417995378f58d88c334f96305a7479
AndyTheDragon <andre.jansam@gmail.com>
2024-12-12 14:00:25 +0100

    added logout

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -6,14 +6,24 @@ import app.persistence.ConnectionPool;
 import app.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
 
 public class UserController
 {
     public static void addRoutes(Javalin app, ConnectionPool dbconnection) {
         app.get("/", ctx -> ctx.render("login.html"));
         app.post("/login", ctx -> login(ctx, dbconnection));
+        app.get("/logout", UserController::logout);
 
     }
+
+    private static void logout(@NotNull Context ctx)
+    {
+        ctx.sessionAttribute("currentUser", null);
+        ctx.req().getSession().invalidate();
+        ctx.redirect("/");
+    }
+
     public static void login(Context ctx, ConnectionPool dbconnection) {
         String email = ctx.formParam("email");
         String password = ctx.formParam("password");
